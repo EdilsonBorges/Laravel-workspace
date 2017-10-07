5ccaf7092ad7e8a63c734e3168c28af94b04602d
Edilson Borges <edilsonfb@gmail.com>
2017-10-07 19:02:57 -0300

    jumbotron

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{$title}}</h1>
-    <p>This is a laravel application</p>
+    <div class="jumbotron text-center">
+        <h1>Hello, world!</h1>
+        <p>This is a laravel application.</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+    </div>
 @endsection
