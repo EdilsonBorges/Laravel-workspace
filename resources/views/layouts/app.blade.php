2b9ba2de4760b62c303adf1a0deb7dfe01c05288
Edilson Borges <edilsonfb@gmail.com>
2017-10-07 18:58:52 -0300

    Including navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>{{config('app.name', 'Laravel')}}</title>
+        <title>{{config('app.name', 'Laravel-Workspace')}}</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="{{asset('css/app.css')}}" rel="stylesheet">
     </head>
     <body>
+        @include('inc.navbar')
         <div class="container">
             @yield('content')
         </div>
