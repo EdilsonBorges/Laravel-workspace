b5d63f8adeba59036d62951c464088bfdf8e2633
Edilson Borges <edilsonfb@gmail.com>
2017-10-07 18:36:40 -0300

    laravel css on layout blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <title>{{config('app.name', 'Laravel')}}</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
+        <link href="{{asset('css/app.css')}}" rel="stylesheet">
     </head>
     <body>
         @yield('content')
