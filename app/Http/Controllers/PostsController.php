94dd5a8c8a67f249f0c4df249ec9717fea32975d
Edilson Borges <edilsonfb@gmail.com>
2017-10-12 18:26:15 -0300

    edit controller and show blade

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -78,7 +78,8 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.edit')->with('post', $post);
     }
 
     /**
