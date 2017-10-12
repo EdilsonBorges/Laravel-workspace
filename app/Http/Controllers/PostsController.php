9823ec41ecb1286269808c0a54318900da53b457
Edilson Borges <edilsonfb@gmail.com>
2017-10-11 21:26:13 -0300

    validation

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -44,7 +44,10 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required'
+        ]);
     }
 
     /**
