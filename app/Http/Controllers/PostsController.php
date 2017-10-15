9f601586ab8a44bbf644373b03541ba71f79a1b7
Edilson Borges <edilsonfb@gmail.com>
2017-10-15 20:25:57 -0200

    delete protection

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -84,6 +84,9 @@ class PostsController extends Controller
     public function edit($id)
     {
         $post = Post::find($id);
+        if(auth()->user()->id !== $post->user_id) {
+            return redirect('/posts')->with('error', 'Unauthorized page');
+        }
         return view('posts.edit')->with('post', $post);
     }
 
@@ -119,6 +122,9 @@ class PostsController extends Controller
     public function destroy($id)
     {
         $post = Post::find($id);
+        if(auth()->user()->id !== $post->user_id) {
+            return redirect('/posts')->with('error', 'Unauthorized page');
+        }
         $post->delete();
         return redirect('/posts')->with('success', 'Post Removed');
     }
