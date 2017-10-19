ba25c22168f52f941650a25d95e86883df0aa326
Edilson Borges <edilsonfb@gmail.com>
2017-10-18 23:57:20 -0200

    new navbar link

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -27,6 +27,7 @@
                 <li><a href="/about">About</a></li>
                 <li><a href="/services">Services</a></li>
                 <li><a href="/posts">Blog</a></li>
+                <li><a href="/posts/create/">Create new Post</a></li>
             </ul>
 
             <!-- Right Side Of Navbar -->
